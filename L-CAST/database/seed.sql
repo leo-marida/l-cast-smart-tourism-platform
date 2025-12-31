530f39540a318de6c050fdc06d864138119e652e
leo-marida <leonardmarida@gmail.com>
2025-12-31 10:20:31 +0200

    added more locations

--- L-CAST/database/seed.sql
+++ L-CAST/database/seed.sql
@@ -17,3 +17,18 @@ INSERT INTO pois (name, region, description, base_popularity_score, location) VA
 ('Tannourine Cedar Reserve', 'Batroun', 'Large cedar forest.', 0.89, ST_SetSRID(ST_MakePoint(35.9262, 34.2084), 4326)),
 ('Zaitunay Bay', 'Beirut', 'Luxury marina.', 0.91, ST_SetSRID(ST_MakePoint(35.4975, 33.9015), 4326)),
 ('Chouf Cedar Reserve', 'Chouf', 'Largest nature reserve.', 0.90, ST_SetSRID(ST_MakePoint(35.6963, 33.6933), 4326));
+
+-- NEW ADDITIONS (North, South, Mount Lebanon, & Beirut)
+INSERT INTO pois (name, region, description, base_popularity_score, location) VALUES 
+('Jeita Grotto', 'Jeita', 'Breathtaking system of crystallized limestone caves.', 0.98, ST_SetSRID(ST_MakePoint(35.6415, 33.9433), 4326)),
+('Cedars of God', 'Bcharre', 'Ancient cedar forest and UNESCO World Heritage site.', 0.96, ST_SetSRID(ST_MakePoint(36.0494, 34.2435), 4326)),
+('Tyre Hippodrome', 'Tyre', 'One of the largest and best-preserved Roman hippodromes.', 0.90, ST_SetSRID(ST_MakePoint(35.2033, 33.2706), 4326)),
+('Batroun Old Souks', 'Batroun', 'Charming coastal town with Phoenician sea wall.', 0.93, ST_SetSRID(ST_MakePoint(35.6644, 34.2497), 4326)),
+('Deir el Qamar', 'Chouf', 'Historic village of stone palaces and mosques.', 0.89, ST_SetSRID(ST_MakePoint(35.5619, 33.7000), 4326)),
+('Taanayel Lake', 'Bekaa', 'Peaceful nature reserve and lake ideal for walking.', 0.86, ST_SetSRID(ST_MakePoint(35.8742, 33.7950), 4326)),
+('Mohammad Al-Amin Mosque', 'Beirut', 'The iconic Blue Mosque in downtown Beirut.', 0.92, ST_SetSRID(ST_MakePoint(35.5056, 33.8947), 4326)),
+('Sursock Museum', 'Beirut', 'Modern art and contemporary culture museum.', 0.87, ST_SetSRID(ST_MakePoint(35.5133, 33.8933), 4326)),
+('Beaufort Castle', 'Nabatieh', 'Crusader fortress offering panoramic views of the south.', 0.81, ST_SetSRID(ST_MakePoint(35.5298, 33.3283), 4326)),
+('Qadisha Valley', 'Bcharre', 'The Holy Valley, deep gorge with ancient monasteries.', 0.91, ST_SetSRID(ST_MakePoint(35.9900, 34.2500), 4326)),
+('Mzaar Kfardebian', 'Kfardebian', 'Largest ski resort in the Middle East.', 0.94, ST_SetSRID(ST_MakePoint(35.8333, 33.9915), 4326)),
+('Tripoli Citadel', 'Tripoli', 'Raymond de Saint-Gilles Citadel overlooking the city.', 0.85, ST_SetSRID(ST_MakePoint(35.8436, 34.4333), 4326));
